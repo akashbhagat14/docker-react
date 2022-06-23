1270f57d299baa2fd389c41c0ec639f482145cdb
akashbhagat14 <akash.bhagat14@gmail.com>
2022-06-24 00:26:45 +0100

    exposed port 80 for nginx

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey there! Welcome to my React JS App ^_^
+          Hey there! Welcome to my ReactJS App, hosted on AWS.
+          Creator : Akash
         </p>
         <a
           className="App-link"
